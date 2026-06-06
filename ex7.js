f73415162ff4c35c0ef83929081a1b13c296b5c0
Scorpion2018JV <joaovitorvianasegobia@gmail.com>
2026-06-05 23:46:43 -0400

    Exercício 7

---
+++ ex7.js
@@ -0,0 +1,25 @@
+class Animal {
+    constructor (nome, som) {
+        this.nome = nome;
+        this.som = som; 
+    }
+
+    emitirSom () {
+        console.log(this.som)
+    }
+}
+
+class Cachorro extends Animal {
+    constructor (nome, raca) {
+        super(nome, 'latido');
+        this.raca = raca;
+    }
+    emitirSom() {
+        console.log(`${this.nome} está emitindo o som ${this.som}`)
+    }
+}
+
+const a1 = new Animal('tiranossaurorex', 'rugido');
+const c1 = new Cachorro('mel','pinscher');
+a1.emitirSom()
+c1.emitirSom()
